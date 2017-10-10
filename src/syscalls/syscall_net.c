a2558329b9db86fc7564ca2329e08bed7b234b6b
skoutsoutos <Stefanos.KOUTSOUTOS@intrasoft-intl.com>
2017-10-10 17:15:53 +0300

    accept syscall impl

--- src/syscalls/syscall_net.c
+++ src/syscalls/syscall_net.c
@@ -110,7 +110,7 @@ static uint32_t connect_io_evt_handler(vm_t* vm, vm_fd_t* fd,
 {
     *remove = 1;
 
-    vm_net_connect_t* io_evt = (vm_net_connect_t*)evt->data;
+    vm_net_cb_t* io_evt = (vm_net_cb_t*)evt->data;
     uint64_t* cb_args = (uint64_t*)deref(io_evt->args, 2 * 8, vm);
     if (!cb_args)
     {
@@ -126,7 +126,7 @@ static uint32_t connect_io_evt_handler(vm_t* vm, vm_fd_t* fd,
         sizeof(net->addr));
     if ((r < 0 && errno != EINPROGRESS) || r == 0)
     {
-        vm_net_connect_t* conn = (vm_net_connect_t*)evt->data;
+        vm_net_cb_t* conn = (vm_net_cb_t*)evt->data;
 
         cb_args[1] = r == 0 ? 0 : errno;
         make_func_procedure(conn->callback, conn->args, 0, vm);
@@ -171,7 +171,8 @@ void sys_connect(vm_t* vm, uint32_t argv, uint32_t retv)
         return;
     }
 
-    vm_net_sock_t* data = malloc(sizeof(vm_net_sock_t));
+    fd->data = malloc(sizeof(vm_net_sock_t));
+    vm_net_sock_t* data = fd->data;
 
     data->addr.sin_family = AF_INET;
     data->addr.sin_addr.s_addr = *(uint32_t*)addr_ptr;
@@ -182,9 +183,9 @@ void sys_connect(vm_t* vm, uint32_t argv, uint32_t retv)
     
     evt->activate = connect_io_evt_handler;
     evt->flags = IO_EVT_SELECT_WRITE;
-    evt->data = malloc(sizeof(vm_net_connect_t));
+    evt->data = malloc(sizeof(vm_net_cb_t));
 
-    vm_net_connect_t* buf = (vm_net_connect_t*)evt->data;
+    vm_net_cb_t* buf = (vm_net_cb_t*)evt->data;
 
     buf->args = *(uint32_t*)(args + 12);
     buf->callback = *(uint32_t*)(args + 16);
@@ -235,8 +236,87 @@ void sys_listen(vm_t* vm, uint32_t argv, uint32_t retv)
 }
 
 
+static uint32_t accept_io_evt_handler(vm_t* vm, vm_fd_t* fd,
+    vm_io_evt_t* evt, int* remove)
+{
+    // continue on accept()ing clients!
+    *remove = 0;
+
+    vm_net_cb_t* io_evt = (vm_net_cb_t*)evt->data;
+    uint64_t* cb_args = (uint64_t*)deref(io_evt->args, 2 * 8, vm);
+    if (!cb_args)
+    {
+        // not much to do...
+        vm->error_no = EFAULT;
+        *remove = 1;
+        return 0;
+    }
+
+    struct sockaddr_in peer_addr;
+    socklen_t len = sizeof(peer_addr);
+    int r = accept(fd->fd, (struct sockaddr*)&peer_addr, &len);
+    if ((r < 0 && errno != EAGAIN) || r >= 0)
+    {
+        vm_net_cb_t* cb = (vm_net_cb_t*)evt->data;
+        if (r >= 0)
+        {
+            uint64_t newfd_idx = alloc_fd(vm);
+            vm_fd_t* newfd = vm->io.fds + newfd_idx;
+            fcntl(r, F_SETFL, O_NONBLOCK);
+
+            newfd->fd = r;
+            newfd->on_close = net_io_close_handler;
+
+            newfd->data = malloc(sizeof(vm_net_sock_t));
+            vm_net_sock_t* data = newfd->data;
+            memcpy(&data->addr, &peer_addr, sizeof(peer_addr));
+
+            cb_args[0] = FD_IDX_TO_HANDLE(newfd_idx);
+        }
+
+        cb_args[1] = r == 0 ? 0 : errno;
+        make_func_procedure(cb->callback, cb->args, 0, vm);
+
+        return 1;
+    }
+
+    // no new clients
+    return 0;
+}
+
+
 void sys_accept(vm_t* vm, uint32_t argv, uint32_t retv)
 {
+    uint8_t* args = deref(argv, 8 + 2 * 4, vm);
+    uint64_t* ret = (uint64_t*)deref(retv, 8, vm);
+    if (!args || !ret)
+    {
+        vm->error_no = EFAULT;
+        if (ret) *ret = 1;
+        return;
+    }
+
+    uint32_t fd_idx = FD_HANDLE_TO_IDX(*(uint64_t*)args);
+    if (fd_idx >= vm->io.n_fds || !vm->io.fds[fd_idx].used)
+    {
+        vm->error_no = EBADF;
+        *ret = 1;
+        return;
+    }
+    vm_fd_t* fd = vm->io.fds + fd_idx;
+
+    vm_io_evt_t* evt = alloc_event(vm, fd);
+    
+    evt->activate = accept_io_evt_handler;
+    evt->flags = IO_EVT_SELECT_READ;
+    evt->data = malloc(sizeof(vm_net_cb_t));
+
+    vm_net_cb_t* buf = (vm_net_cb_t*)evt->data;
+
+    buf->args = *(uint32_t*)(args + 8);
+    buf->callback = *(uint32_t*)(args + 12);
+
+    *ret = 0;
 }
 
 
--- src/syscalls/syscall_net.h
+++ src/syscalls/syscall_net.h
@@ -12,26 +12,19 @@
 #define LISTEN_BACKLOG  10
 
 
-#define NET_SOCK_ACCEPTING  0x1
-#define NET_SOCK_CONNECTING 0x2
-#define NET_SOCK_CONNECTED  0x4
-
-
 typedef struct _vm_net_sock_t
 {
     struct sockaddr_in addr;
-
-    uint16_t flags;
 }
 vm_net_sock_t;
 
 
-typedef struct _vm_net_connect_t
+typedef struct _vm_net_cb_t
 {
     uint32_t callback;
     uint32_t args;
 }
-vm_net_connect_t;
+vm_net_cb_t;
 
 
 #endif
