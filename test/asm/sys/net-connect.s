a4f3b50eabbcca9fa995214db8eb72797082d28b
skoutsoutos <Stefanos.KOUTSOUTOS@intrasoft-intl.com>
2017-10-11 13:35:21 +0300

    socket/connect syscalls test

---
+++ test/asm/sys/net-connect.s
@@ -0,0 +1,40 @@
+
+%data @ok           0x1122334455667788 /8
+%data @net_ip		0x11223344
+%data @net_port		0x1122
+%data @fd           0x0 /8
+%data @err          0x0 /8
+%data @test         0x0 /8
+%data @scall_args	0x0 /64
+%data @c_cb_args	0x0 /16
+
+_entry:
+	; socket()
+    cp64        scall_args  1
+    syscall 15  &scall_args &fd
+    eq          test        fd 0
+    jtrue       &on_error   test
+	
+	; connect()
+	write64		&scall_args		fd
+	write64		&scall_args	8	&net_ip
+	write64		&scall_args 12	&c_cb_args
+	write64		&scall_args 16	&on_connect
+	syscall 17	&scall_args	&err
+    ne          test        err     0
+    jtrue       &on_error   test
+
+	ret ; exit main process
+
+on_connect:
+	jmp	&socket_close
+	; ret
+
+socket_close:
+	; close()
+    syscall 5   &fd         &ok
+    ret
+
+on_error:
+    cp64        ok          0x1122
+    ret
--- test/test_vm_sys_net.c
+++ test/test_vm_sys_net.c
@@ -1,7 +1,7 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
 #include "tests.h"
 #include "vm.h"
@@ -9,10 +9,45 @@
 
 #define PRG_PATH    "/test/asm/sys/"
 
+struct sockaddr_in google_addr;
+
+
+static void test_net_connect()
+{
+    vm_t* vm = mk_vm_for_asm(xstr(PROJECT_ROOT) PRG_PATH
+        "net-connect.s");
+    
+    *(uint32_t*)(vm->memory + 8) = google_addr.sin_addr.s_addr;
+    *(uint16_t*)(vm->memory + 12) = google_addr.sin_port;
+
+    execute_vm(vm);
+    print_vm_state(vm);
+
+    assert(vm->exceptions == 0);
+    assert(vm->procedures == NULL);
+    assert(vm->error_no == 0);
+    assert(vm->io.used_fds == 0);
+    assert(*((uint64_t*)vm->memory) == 0);
+    assert(*((uint64_t*)(vm->memory + 14)) > 0); // the fd
+
+    destroy_vm(vm);
+}
+
 
 int main(int argc, char** argv)
 {
+    struct addrinfo* result;
+    int err = getaddrinfo("www.google.com", NULL, NULL, &result);
+    assert(err == 0);
+    assert(result != NULL);
+
+    google_addr = *(struct sockaddr_in*)result->ai_addr;
+
+    freeaddrinfo(result);
+
     initialize_engine();
+
+    test_net_connect();
 
     return 0;
 }
